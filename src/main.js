af46f5eadb0a09be454768bf616a25389d3675be
onkarabile masoko <onkarabilemasoko1@gmail.com>
2024-08-21 11:35:10 +0200

    chore: Initialize Vue app with routing, state management, and external styles
     Set up the Vue application with `createApp` and mounted the root component.
     Integrated Vue Router for client-side routing by importing and using the router instance.
     Added Pinia for state management and initialized it within the app.
     Included external styles and icons by importing SweetAlert2 and Bootstrap Icons CSS files.
     Ensured the app is fully configured and ready for development.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import routing from './router';
+import { createPinia } from 'pinia'; 
+import 'sweetalert2/dist/sweetalert2.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
-createApp(App).mount('#app')
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia);
+app.use(routing);
+app.mount('#app');
