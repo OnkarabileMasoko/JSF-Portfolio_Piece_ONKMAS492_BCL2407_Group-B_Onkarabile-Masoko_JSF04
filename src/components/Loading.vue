e9deff729b0481206c69d95abd57f162cf8ee697
onkarabile masoko <onkarabilemasoko1@gmail.com>
2024-08-21 10:22:30 +0200

    Deleted HelloWorld.vue and worked on loading state

---
+++ src/components/Loading.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-for="n in 20" class="max-w-screen-xl mx-auto mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div class="bg-gray-200 p-4 rounded shadow animate-pulse">
+      <div class="bg-gray-300 h-48 mt-3 rounded"></div>
+      <div class="h-6 bg-gray-300 mt-4 rounded"></div>
+      <div class="h-4 bg-gray-300 mt-2 rounded"></div>
+      <div class="h-4 bg-gray-300 mt-2 rounded"></div>
+      <div class="h-6 bg-gray-300 mt-4 w-1/2 rounded"></div>
+    </div>
+  </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'LoadingComponent',
+  };
+  </script>
+  
+  <style scoped>
+  </style>
+  
